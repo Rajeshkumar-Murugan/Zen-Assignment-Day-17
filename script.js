29793cd10fb93adfb7da17e5587d1828c907e53c
Rajeshkumar-Murugan <85731833+Rajeshkumar-Murugan@users.noreply.github.com>
2021-09-18 03:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+setTimeout(()=> {  
+    setTimeout(()=> { 
+        setTimeout(()=> {  
+            setTimeout(()=> {  
+                setTimeout(()=> { 
+                    setTimeout(()=> {  
+                        setTimeout(()=> { 
+                            setTimeout(()=> {  
+                                setTimeout(()=> {  
+                                    setTimeout(()=> { 
+                                        setTimeout(()=> { 
+                                        
+                                        document.body.innerText="Happy Independence Day ";
+                                        console.log("This message is shown after 11 seconds");}, 1000);
+                      
+                                    document.body.innerText="1";
+                                    console.log("This message is shown after 10 seconds");}, 1000);
+                    
+                                document.body.innerText="2";
+                                console.log("This message is shown after 9 seconds");}, 1000);
+                  
+                            document.body.innerText="3";
+                            console.log("This message is shown after 8 seconds");}, 1000);
+                
+                        document.body.innerText="4"; 
+                        console.log("This message is shown after 7 seconds");}, 1000);
+              
+                    document.body.innerText="5";
+                    console.log("This message is shown after 6 seconds");}, 1000);
+        
+                document.body.innerText="6";
+                console.log("This message is shown after 5 seconds");}, 1000);
+          
+            document.body.innerText="7";
+            console.log("This message is shown after 4 seconds");}, 1000);
+        
+        document.body.innerText="8";
+        console.log("This message is shown after 3 seconds");}, 1000);
+      
+    document.body.innerText="9";
+     console.log("This message is shown after 2 seconds");}, 1000);
+    
+document.body.innerText="10";
+console.log("This message is shown after 1 seconds");}, 1000);
+
+document.body.setAttribute("style", "text-align:center; height:100vh; display: flex; font-size: 3em; height: 100vh; flex-direction: column; align-items: center; justify-content: space-around;"); 
